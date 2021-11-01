dd7e216a2e373d6b966305438d88f0fd29c2576b
matthewogtong <ogtongm@gmail.com>
2021-11-01 11:26:14 -0400

    Fix viewBinding declaration

--- calculatorkt/src/main/java/com/example/calculatorkt/HomeActivity.kt
+++ calculatorkt/src/main/java/com/example/calculatorkt/HomeActivity.kt
@@ -2,13 +2,16 @@ package com.example.calculatorkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.calculatorkt.databinding.ActivityCalculatorBinding
 
 class HomeActivity : AppCompatActivity() {
 
-    private
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityCalculatorBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_calculator)
     }
 }
