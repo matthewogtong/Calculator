129402f91de9c87b41655c8cb6432f5ecb5ee8c1
matthewogtong <ogtongm@gmail.com>
2021-11-01 11:47:41 -0400

    Initialize and declare calculation and result variables -kt

--- calculatorkt/src/main/java/com/example/calculatorkt/HomeActivity.kt
+++ calculatorkt/src/main/java/com/example/calculatorkt/HomeActivity.kt
@@ -2,15 +2,23 @@ package com.example.calculatorkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
 import com.example.calculatorkt.databinding.ActivityCalculatorBinding
 
 class HomeActivity : AppCompatActivity() {
 
         private lateinit var binding: ActivityCalculatorBinding
+        private lateinit var calculation: EditText; lateinit var result: EditText
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculatorBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        calculation = binding.calculation
+        result = binding.result
     }
 }
