876cce1734d9bbf40d61ae353bc363a4ce95b117
matthewogtong <ogtongm@gmail.com>
2021-11-01 11:19:51 -0400

    Add buildFeatures to set true to viewBinding -kt

--- calculatorkt/src/main/java/com/example/calculatorkt/HomeActivity.kt
+++ calculatorkt/src/main/java/com/example/calculatorkt/HomeActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class HomeActivity : AppCompatActivity() {
+
+    private
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_calculator)
     }
 }
